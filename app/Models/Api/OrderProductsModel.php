7b38148c17321f0ffe48779703063958213a7959
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-07-13 17:07:10 +0300

    fix

--- app/Models/Api/OrderProductsModel.php
+++ app/Models/Api/OrderProductsModel.php
@@ -9,9 +9,13 @@ class OrderProductsModel extends Model
     protected $table = 'ci_bl_order_products';
     protected $primaryKey = 'id';
 
-    public function getTotalProductPrices($orderId)
+
+    // [ORDER PRODUCTS TOTAL WITHOUT VAT] 
+    // Calculate product prices (all product prices from a specific order without VAT (net price))
+
+    public function getTotalWithoutVat($orderId)
     {
-        $this->select('price_brutto, tax_rate', 'quantity');
+        $this->select('price_brutto, tax_rate, quantity');
         $this->where('order_id', $orderId);
         $products = $this->findAll();
 
@@ -23,8 +27,35 @@ class OrderProductsModel extends Model
         return $total;
     }
 
-    private function calculatePriceWithoutVAT($price, $vatRate,$quantity)
+    private function calculatePriceWithoutVAT($price, $vatRate, $quantity)
     {
         return ($price / (1 + $vatRate/100)) * $quantity;
     }
+
+
+
+    // [ORDER PRODUCTS TOTAL WITH VAT] 
+    // Calculate product prices (all product prices from a specific order including VAT (brut price))
+
+    public function getTotalWithVat($orderId)
+    {
+        $this->select('price_brutto, quantity');
+        $this->where('order_id', $orderId);
+        $products = $this->findAll();
+
+        $total = 0;
+        foreach ($products as $product) {
+            $total += $this->calculatePriceWithVAT($product['price_brutto'], $product['quantity']);
+        }
+
+        return $total;
+    }
+
+
+    private function calculatePriceWithVAT($price, $quantity)
+    {
+        return $price * $quantity;
+    }
+
+    
 }
--- app/Models/Api/OrdersModel.php
+++ app/Models/Api/OrdersModel.php
@@ -9,22 +9,28 @@ class OrdersModel extends Model
     protected $table = 'ci_bl_orders';
     protected $primaryKey = 'id';
 
+
+    // Returning order list (including total with vat and without)
+    // Used in Orders Controller for Endpoint "getAll"
     public function getOrdersList()
     {
-        $this->select('id, delivery_price');
+        $this->select('id, invoice_company');
         $orders = $this->findAll();
 
         $orderProductsModel = new \App\Models\Api\OrderProductsModel();
 
         foreach ($orders as &$order) {
-            $totalProductPrices = $orderProductsModel->getTotalProductPrices($order['id']);
-            $order['totalNoVat'] = $this->calculatePriceWithoutVAT($order['delivery_price'], $order['tax_rate']) + $totalProductPrices;
+            $totalProductPricesVAT = $orderProductsModel->getTotalWithoutVat($order['id']);
+            $totalProductPricesNOVAT = $orderProductsModel->getTotalWithVat($order['id']);
+
+            $order['totalNoVat'] = $this->calculateShippingWithoutVat($order['delivery_price'], $order['tax_rate']) + $totalProductPricesVAT;
+            $order['totalWithVat'] = $order['delivery_price'] + $totalProductPricesNOVAT;
         }
 
         return $orders;
     }
 
-    private function calculatePriceWithoutVAT($price, $vatRate)
+    private function calculateShippingWithoutVat($price, $vatRate)
     {
         return $price / (1 + $vatRate/100);
     }
