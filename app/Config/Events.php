156d338f12cd3a68799a4ad1cd6d5a9bdad2512d
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-07-14 22:53:37 +0300

    asd

--- app/Config/Events.php
+++ app/Config/Events.php
@@ -36,7 +36,7 @@ Events::on('pre_system', static function () {
 
         // Headers for CORS
 
-        header('Access-Control-Allow-Origin: http://whf.ybomedia.ro:3000');
+        header('Access-Control-Allow-Origin: http://whf.ybomedia.ro:3000'); // save
         header('Access-Control-Allow-Origin: http://localhost:3000');
         header("Access-Control-Allow-Methods: *");
         header("Access-Control-Allow-Headers: *");
