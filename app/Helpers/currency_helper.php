ebac69528df91b87528427ad392bf04bc6c20e6f
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-07-18 19:59:55 +0300

    Fix currencies

--- app/Helpers/currency_helper.php
+++ app/Helpers/currency_helper.php
@@ -1,20 +1,24 @@
 <?php
 
+if (! function_exists('getCurrencyRate')) {
+    function getCurrencyRate($currencyCode)
+    {
+        $xml = simplexml_load_file('https://www.bnr.ro/nbrfxrates.xml');
 
-if (! function_exists('getCurrencyRate')){
-function getCurrencyRate($currencyCode)
-{
-    $xml = simplexml_load_file('https://www.bnr.ro/nbrfxrates.xml');
-    $namespaces = $xml->getNamespaces(true);
-    $body = $xml->children($namespaces['Body']);
-    $cube = $body->Cube;
+        // Namespace required to properly parse the XML
+        $namespaces = $xml->getNamespaces(true);
+        $xml->registerXPathNamespace('d', $namespaces['']);
 
-    foreach ($cube->children() as $rate) {
-        if ((string)$rate['currency'] === strtoupper($currencyCode)) {
-            return (float)$rate;
+        // XPath query to directly access the 'Rate' elements
+        $rates = $xml->xpath("//d:Rate");
+
+        foreach ($rates as $rate) {
+            if ((string)$rate['currency'] === strtoupper($currencyCode)) {
+                return (float)$rate;
+            }
         }
-    }
 
-    // Return null if the currency was not found in the XML
-    return null;
-}}
+        // Return null if the currency was not found in the XML
+        return null;
+    }
+}
