7645601cc6919d255d3eae3e43aff57fb17ddc1e
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-07-19 15:44:29 +0300

    Small fix

--- app/Models/Api/PurchaseOrder/PurchaseOrderProductModel.php
+++ app/Models/Api/PurchaseOrder/PurchaseOrderProductModel.php
@@ -109,7 +109,7 @@ class PurchaseOrderProductModel extends Model
         $stockModel = new \App\Models\Api\Inventory\StockModel();
         $purchaseOrderModel = new \App\Models\Api\PurchaseOrder\PurchaseOrderModel();
 
-        $invoice = $purchaseOrderModel->find($invoiceId);
+        $invoice = $purchaseOrderModel->where('invoice_id', $invoiceId)->first();
         $supplierId = $invoice['supplier'];
 
 
