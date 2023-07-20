576fc111b4bd56ebd03f9a6e1ca2a6b9c4de403e
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-07-20 23:09:39 +0300

    fix

--- app/Models/Api/PurchaseOrder/PurchaseOrderModel.php
+++ app/Models/Api/PurchaseOrder/PurchaseOrderModel.php
@@ -83,19 +83,17 @@ class PurchaseOrderModel extends Model
     }
 
     public function modifyInvoice($requestData){
-
-        $invoiceId = $requestData['invoice_id'];
         
-        $invoice = $this->find($invoiceId);
+        $invoice = $this->find($requestData['invoice_id']);
 
         if($invoice === null) {
             // The invoice does not exist
-            return ['invoice_id' => $invoiceId, 'status' => 'missing'];
+            return ['invoice_id' => $requestData['invoice_id'], 'status' => 'missing'];
 
         }
 
         if ($invoice['locked'] == 1) {
-            return ['status' => 'locked', 'invoice_id' => $invoiceId];
+            return ['status' => 'locked', 'invoice_id' => $requestData['invoice_id']];
         }
 
         
@@ -103,10 +101,10 @@ class PurchaseOrderModel extends Model
         if (isset($requestData['invoice_number'])){
 
             $this->set('number', $requestData['invoice_number'])
-                 ->where('invoice_id', $invoiceId)
+                 ->where('invoice_id', $requestData['invoice_id'])
                  ->update();
 
-             return ['invoice_id' => $invoiceId, 'status' => 'succes'];
+             return ['invoice_id' => $requestData['invoice_id'], 'status' => 'succes'];
 
         }
 
@@ -116,17 +114,17 @@ class PurchaseOrderModel extends Model
                  ->where('invoice_id', $requestData['invoice_id'])
                  ->update();
 
-             return ['invoice_id' => $invoiceId, 'status' => 'succes'];
+             return ['invoice_id' => $requestData['invoice_id'], 'status' => 'succes'];
 
         }
 
         if (isset($requestData['invoice_date'])){
 
             $this->set('number', $requestData['invoice_date'])
-                 ->where('invoice_id', $invoiceId)
+                 ->where('invoice_id', $requestData['invoice_id'])
                  ->update();
 
-             return ['invoice_id' => $invoiceId, 'status' => 'succes'];
+             return ['invoice_id' => $requestData['invoice_id'], 'status' => 'succes'];
 
         }
 
