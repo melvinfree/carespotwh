71fd662eff31fbfd5bb89ab80bcf806529a95cfb
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-07-25 16:52:23 +0300

    asd

--- app/Models/Api/PurchaseOrder/PurchaseOrderProductModel.php
+++ app/Models/Api/PurchaseOrder/PurchaseOrderProductModel.php
@@ -27,6 +27,7 @@ class PurchaseOrderProductModel extends Model
             invoices_in_products.acquisition_price * invoices_in.currency_rate as price_ron,
             invoices_in_products.acquisition_price,
             invoices_in.currency,
+            invoices_in.quantity,
             invoices_in_products.acquisition_price * invoices_in.currency_rate * invoices_in_products.quantity as total_no_vat
         FROM 
             invoices_in_products
