1a48ea4c03f6664c1c34b2de72715280c0f1124d
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-08-24 19:59:39 +0300

    Fix StockModel to not allow adding more products in stock table than quantity in purchase order

--- app/Models/Api/Inventory/StockModel.php
+++ app/Models/Api/Inventory/StockModel.php
@@ -29,6 +29,15 @@ class StockModel extends Model
     
     public function addToStock($data)
     {
+
+        $countProductsAdded = $this->where('product_id', $data['product_id'])
+                ->where('invoice_product_id', $data['invoice_product_id'])
+                ->where('invoice_in_id', $data['invoice_in_id'])
+                ->countAllResults();
+
+        if($data['quantity'] >= $countProductsAdded){
+                    return ["error" => false, "message" => "All products added"];
+        }
         
         for($i = 0; $i < $data['quantity']; $i++) {
             $this->insert([
@@ -51,5 +60,14 @@ class StockModel extends Model
             ->where('status', 'instock')
             ->countAllResults();
     }
+
+
+    private function getProductsAddedToStock($invoice_product_id,$invoice_in_id)
+    {
+        return $this->where('invoice_product_id', $invoice_product_id)
+            ->where('invoice_in_id', $invoice_in_id)
+            ->countAllResults();
+    }
+
 
 }
