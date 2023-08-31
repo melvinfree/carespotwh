f0180a56af6d9010cfecefd7b1af0380c4ea1b23
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-08-31 22:00:21 +0300

    PushToGit (Warehouse Model & Controller)

---
+++ app/Controllers/Api/General/Warehouse.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Controllers\Api\General;
+
+use CodeIgniter\API\ResponseTrait;
+use App\Models\Api\Inventory\ProductsModel;
+use CodeIgniter\Controller;
+use App\Models\Api\PurchaseOrder\PurchaseOrderModel;
+use App\Models\Api\PurchaseOrder\PurchaseOrderProductModel;
+use App\Models\Api\Inventory\ReceptionsModel;
+use App\Models\Api\Inventory\TransfersModel;
+use App\Models\Api\Inventory\StockModel;
+use App\Models\Api\Inventory\WarehouseModel;
+use DateTime;
+
+class Warehouse extends Controller
+{
+    use ResponseTrait;
+
+    public function __construct()
+    {
+        helper("api");
+        helper("currency");
+    }
+
+    public function warehouseList()
+    {
+        $WarehouseModel = new WarehouseModel();
+
+        // Validate token and get the request body
+        try {
+            $requestData = validateTokenAndFetchData();
+        } catch (\Exception $e) {
+            return $this->failUnauthorized($e->getMessage());
+        }
+
+        // Validate the JSON payload
+        $expectedKeys = ["limit", "offset"];
+        $requestDataKeys = array_keys($requestData);
+
+        if (
+            count($requestData) !== 2 ||
+            !empty(array_diff($expectedKeys, $requestDataKeys))
+        ) {
+            return $this->fail("Invalid JSON Payload, check APIDocs.", 400);
+        }
+        if (!is_int($requestData["limit"]) || !is_int($requestData["offset"])) {
+            return $this->fail("Invalid JSON Payload, check APIDocs.", 400);
+        }
+
+        
+        $jsonRes = json_encode(succesResponse($WarehouseModel->whList($requestData["limit"], $requestData["offset"])), true);
+
+        return $this->respond($jsonRes, 200);
+    }
+    
+    // ALLOWED FIELDS
+    /* MANDATORY: warehouse_id
+
+    */
+    public function getTransfer()
+    {
+        $WarehouseModel = new WarehouseModel();
+
+        // Validate token and get the request body
+        try {
+            $requestData = validateTokenAndFetchData();
+        } catch (\Exception $e) {
+            return $this->failUnauthorized($e->getMessage());
+        }
+
+        // Validate the JSON payload
+        $expectedKeys = ["warehouse_id"];
+        $requestDataKeys = array_keys($requestData);
+
+        if (
+            count($requestData) !== 1 || !empty(array_diff($expectedKeys, $requestDataKeys))
+        ) {
+            return $this->fail("Invalid JSON Payload, check APIDocs.", 400);
+        }
+        if (!is_int($requestData["transfer_id"])) {
+            return $this->fail("Invalid JSON Payload, check APIDocs.", 400);
+        }
+
+        
+        
+        $results = $WarehouseModel->getWHInfo($requestData["warehouse_id"]);
+
+        if ($results) {
+            $jsonRes = json_encode(succesResponse($results), true);
+            return $this->respond($jsonRes, 200);
+        } else {
+            return $this->failNotFound('No Transfer found with ID: '.$requestData["warehouse_id"]);
+        }
+
+    }
+    // ALLOWED FIELDS
+    /* MANDATORY: warehouse_id
+
+    */
+    public function deleteWarehouse()
+    {
+    $WarehouseModel = new WarehouseModel();
+
+    // Validate token and get the request body
+    try {
+        $requestData = validateTokenAndFetchData();
+    } catch (\Exception $e) {
+        return $this->failUnauthorized($e->getMessage());
+    }
+    $responses = $WarehouseModel->deleteWH($requestData['warehouse_id']);
+
+    $jsonRes = json_encode(succesResponse($responses), true);
+
+    return $this->respond($jsonRes, 200);
+    }
+    
+
+    // ALLOWED FIELDS
+    /*
+    MANDATORY: "warehouse_id"
+    "name", 
+    "comments", 
+    "status", 
+    "allow_servicing_stock", 
+    "open_box", 
+    "location_id"
+    */
+    public function setWarehouseInfo()
+    {
+    $WarehouseModel = new WarehouseModel();
+
+    // Validate token and get the request body
+    try {
+        $requestData = validateTokenAndFetchData();
+    } catch (\Exception $e) {
+        return $this->failUnauthorized($e->getMessage());
+    }
+    $responses = $WarehouseModel->modifyWarehouse($requestData);
+
+    $jsonRes = json_encode(succesResponse($responses), true);
+
+    return $this->respond($jsonRes, 200);
+    }
+
+}
--- app/Models/Api/Inventory/WarehouseModel.php
+++ app/Models/Api/Inventory/WarehouseModel.php
@@ -10,6 +10,8 @@ class WarehouseModel extends Model
     protected $table = 'warehouses';
     protected $primaryKey = 'id';
 
+    protected $allowedFields = ["name", "comments", "status", "allow_servicing_stock", "open_box", "location_id"];
+
 
     public function getWarehouseList()
     {
@@ -18,6 +20,118 @@ class WarehouseModel extends Model
                 ->findAll();
 
     return $products;
+    }
+
+    public function whList($limit,$ofset)
+    {
+    $warehouses = $this->select('*') 
+                 ->limit($limit,$ofset)
+                ->findAll();
+
+    return $warehouses;
+    }
+
+    public function getWHInfo($warehouse_id)
+    {
+        $warehouse = $this->select('*')
+                    ->where('id', $warehouse_id)
+                    ->first();
+
+        if ($warehouse) {
+            return $warehouse;
+        }
+        else{
+            return ["error" => true, "message" => "This warehouse dosen't exist"];
+        }
+    }
+
+    public function deleteWH($warehouse_id)
+    {
+        
+        $stockModel = new \App\Models\Api\Inventory\StockModel();
+
+        $warehouse = $this->find($warehouse_id);
+
+        if(!$warehouse){
+            return ["error" => true, "message" => "This warehouse does not exist"];
+        }
+        
+        // Check if in that warehouse exist products, if yes, cannot be deleted
+        $count_products = $stockModel->where('warehouse', $warehouse_id)
+                                     ->countAllResults();
+
+        if($count_products > 0){
+            return ["error" => true, "message" => "This warehouse cannot be deleted because we have products there(sold or not sold)"];
+        } 
+
+        // Delete product from current NIR.
+        $this->delete(['id' => $warehouse_id]);
+
+    }
+
+    public function modifyWarehouse($requestData)
+    {
+        $warehouse = $this->find($requestData["warehouse_id"]);
+
+        if ($warehouse === null) {
+            // The invoice does not exist
+            return [
+                "error" => true,
+                "warehouse_id" => $requestData["warehouse_id"],
+                "message" => "This warehouse does not exist",
+            ];
+        }
+
+        $response[] = ["warehouse_id" => $requestData["warehouse_id"]];
+
+        if (isset($requestData["name"])) {
+            $this->set("name", $requestData["name"])
+                ->where("id", $requestData["warehouse_id"])
+                ->update();
+
+            $response["warehouse_name"] = ["error" => false, "message" => "Name updated"];
+        }
+
+        if (isset($requestData["comments"])) {
+            $this->set("comments", $requestData["comments"])
+                ->where("id", $requestData["warehouse_id"])
+                ->update();
+
+            $response["warehouse_comments"] = ["error" => false, "message" => "Comments updated"];
+        }
+
+        if (isset($requestData["status"])) {
+            $this->set("status", $requestData["status"])
+                ->where("id", $requestData["warehouse_id"])
+                ->update();
+
+            $response["warehouse_status"] = ["error" => false, "message" => "Status updated"];
+        }
+
+        if (isset($requestData["allow_servicing_stock"])) {
+            $this->set("allow_servicing_stock", $requestData["allow_servicing_stock"])
+                ->where("id", $requestData["warehouse_id"])
+                ->update();
+
+            $response["warehouse_allow_servicing_stock"] = ["error" => false, "message" => "allow_servicing_stock updated"];
+        }
+        if (isset($requestData["open_box"])) {
+            $this->set("open_box", $requestData["open_box"])
+                ->where("id", $requestData["warehouse_id"])
+                ->update();
+
+            $response["warehouse_open_box"] = ["error" => false, "message" => "open_box updated"];
+        }
+
+        if (isset($requestData["location_id"])) {
+            $this->set("location_id", $requestData["location_id"])
+                ->where("id", $requestData["warehouse_id"])
+                ->update();
+
+            $response["warehouse_location_id"] = ["error" => false, "message" => "location_id updated"];
+        }
+
+        return $response;
     }
 
     public function getAllowServicingStockWarehouseList()
