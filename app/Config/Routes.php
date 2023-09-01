a91a306607cd4b24231202c7f863394b38eaf6ce
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-09-01 15:20:59 +0300

    asd

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -96,7 +96,7 @@ $routes->post('/Api/Warehouse/setWarehouseInfo', '\App\Controllers\Api\Warehouse
 
 // PickPack Module
 
-$routes->post('/Api/Pickpack/pickProduct', '\App\Controllers\Api\Pickpack::scanProduct');
+$routes->post('/Api/Pickpack/scanProduct', '\App\Controllers\Api\Pickpack::scanProduct');
 
 /*
  * --------------------------------------------------------------------
