d62ce9951ab6a025087b09c50a8488305a323402
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-09-01 15:32:30 +0300

    asd

---
+++ app/Controllers/Api/Pickpack.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controllers\Api;
+
+use CodeIgniter\API\ResponseTrait;
+use App\Models\Api\Inventory\ProductsModel;
+use CodeIgniter\Controller;
+use App\Models\Api\PurchaseOrder\PurchaseOrderModel;
+use App\Models\Api\PurchaseOrder\PurchaseOrderProductModel;
+use App\Models\Api\Inventory\ReceptionsModel;
+use App\Models\Api\Inventory\TransfersModel;
+use App\Models\Api\Inventory\StockModel;
+use App\Models\Api\Scanning\OrderBoxProductsModel;
+use DateTime;
+
+class Pickpack extends Controller
+{
+    use ResponseTrait;
+
+    public function __construct()
+    {
+        helper("api");
+        helper("currency");
+    }
+
+
+    public function scanProduct() {
+
+        $OrderBoxProductsModel = new OrderBoxProductsModel();
+    
+        // Validate token and get the request body
+        try {
+            $requestData = validateTokenAndFetchData();
+        } catch (\Exception $e) {
+            return $this->failUnauthorized($e->getMessage());
+        }
+
+        $result = $OrderBoxProductsModel->processProduct($requestData);
+
+        $jsonRes = json_encode(succesResponse($result), true);
+
+        return $this->respond($jsonRes, 200);
+
+    }
+
+}
