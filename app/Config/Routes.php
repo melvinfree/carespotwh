15083eb1ec0cd18ce80646161ed049556b434cc5
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-09-06 22:25:20 +0300

    Push Route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -63,6 +63,10 @@ $routes->post('/Api/Purchase/setInvoiceValues', '\App\Controllers\Api\Purchase::
 $routes->post('/Api/Purchase/getInvoicePList', '\App\Controllers\Api\Purchase::getInvoicePList');
 $routes->post('/Api/Purchase/getPurchase', '\App\Controllers\Api\Purchase::getPurchase');
 $routes->post('/Api/Purchase/lockInvoice', '\App\Controllers\Api\Purchase::lockInvoice');
+$routes->post('/Api/Purchase/reversePurchase', '\App\Controllers\Api\Purchase::reversePurchase');
+
+
+
 // Inventory Api Routes - Endpoints
 
 $routes->post('/Api/Inventory/getReceptionsList', '\App\Controllers\Api\Inventory::getReceptionsList');
