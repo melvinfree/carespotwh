ccd88d5f3cb5efdefb14a9d339bf992c0282168e
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-09-08 21:51:07 +0300

    asd

--- app/Controllers/Api/Transfers.php
+++ app/Controllers/Api/Transfers.php
@@ -154,9 +154,8 @@ class Transfers extends Controller
 
     $responses = $TransfersModel->insertProducts($requestData);
 
-    return $requestData;
 
-    $jsonRes = json_encode(succesResponse($responses), true);
+    $jsonRes = json_encode(succesResponse($requestData), true);
 
     return $this->respond($jsonRes, 200);
     }
