e1537528ecab460d3dcb73b8439d740d12beac3c
melvinfree <135867408+melvinfree@users.noreply.github.com>
2023-09-08 14:24:31 +0300

    asd

--- app/Models/Api/PurchaseOrder/PurchaseOrderProductModel.php
+++ app/Models/Api/PurchaseOrder/PurchaseOrderProductModel.php
@@ -197,14 +197,14 @@ class PurchaseOrderProductModel extends Model
 
         
 
-        $ProductsToBeReversed = $stockModel->where('invoice_in_id', $initial_invoice_id['id'])
+        /*$ProductsToBeReversed = $stockModel->where('invoice_in_id', $initial_invoice_id['id'])
                                                 ->where('invoice_in_storno_id' , $data['invoice_id'])
                                                 ->where('invoice_in_product_storno_id', $insertId)
                                                 ->where('product_id', $reversalProduct['product_id'])
                                                 ->where('status', 'reversesale_supplier')
                                                 ->limit($limit)
                                                 ->get()    
-                                                ->getResultArray();                                
+                                                ->getResultArray();      */                          
                                                 
                         
 
